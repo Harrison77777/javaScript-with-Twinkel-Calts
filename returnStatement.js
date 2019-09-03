4a4d568fbde2f819682d374e6147e81b80ebe565
harrison77777 <ermhroy7@gmail.com>
2019-09-03 09:17:02 +0600

    added some codes in this returnStatement file

---
+++ returnStatement.js
@@ -0,0 +1,6 @@
+function person (name, age){
+    return name +"'s age is "+ age;
+}
+var person = person;
+console.log(person('shahin', 22));
+/* Note: If you return any think into a function then if want to printout that , so then you have to declare that function into a console.log brackets  */
