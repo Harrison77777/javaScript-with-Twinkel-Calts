f3da60348e4210c9f0b0ed58a3a92464653d82d3
harrison77777 <ermhroy7@gmail.com>
2019-09-15 23:37:37 +0600

    made some changes

--- returnStatement.js
+++ returnStatement.js
@@ -1,6 +1,11 @@
 function person (name, age){
     return name +"'s age is "+ age;
 }
+
 var person = person;
 console.log(person('shahin', 22));
+
+var person = person('harrison', 27)
+console.log(person);
+
 /* Note: If you return any think into a function then if want to printout that , so then you have to declare that function into a console.log brackets  */
